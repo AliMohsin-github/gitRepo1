2f7cbe8497718c33ccef9ac27b0a82512fb3ecfe
AliMohsin-github <sayed.mohsin009@gmail.com>
2019-08-27 16:35:00 +0530

    file has been modified

--- src/main/java/com/git/java/removeConflict.java
+++ src/main/java/com/git/java/removeConflict.java
@@ -7,20 +7,20 @@ public class removeConflict {
 		System.out.println("wellcome to GIT Tutorial Sayed");		
 		System.out.println("wellcome to GIT Tutorial Mohsin");
 		System.out.println("wellcome to GIT Tutorial Ali");
-		
+
 		int a = 10;
 		int b = 20;
-		
+
 		int c = a + b;
-		
+
 		System.out.println("Dear Mohsin Value of addition is:" +c );
-		
+
 		setMessage();
-		
+
 	}
-	
+
 	public static void setMessage() {
-		
+
 		System.out.println("Test message to resolve Merge conflict in GIT");
 	}
 
