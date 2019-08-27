61dbb00bd27cf9d1c8f1bd2996020c594b00ac91
AliMohsin-github <sayed.mohsin009@gmail.com>
2019-08-27 12:30:26 +0530

    file modified from Dev Env

--- src/main/java/com/git/java/removeConflict.java
+++ src/main/java/com/git/java/removeConflict.java
@@ -7,15 +7,23 @@ public class removeConflict {
 		System.out.println("wellcome to GIT Tutorial Sayed");		
 		System.out.println("wellcome to GIT Tutorial Mohsin");
 		System.out.println("wellcome to GIT Tutorial Ali");
-		
+
 		int a = 10;
 		int b = 20;
-		
+
 		int c = a + b;
-		
+
 		System.out.println("Value of addition is:" +c );
 
+		setMessage1();
 
 	}
+
+
+	public static void setMessage1() {
+
+		System.out.println("Resolve Merge conflict in GIT");
+	}
+
 
 }
